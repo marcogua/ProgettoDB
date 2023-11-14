2336e51b3f8f2f1816c98dc4b05f2de59125782e
mrvittoriosomma <vittorio.somma1999@gmail.com>
2023-11-14 08:52:40 +0100

    Aggiunte le chiavi primarie ed esterne

--- portafoglio.sql
+++ portafoglio.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE portafoglio(
     --ID_PORTAFOLGIO identifica univocamente un portafoglio
-    id_portafoglio int NOT NULL,
+    id_portafoglio int PRIMARY KEY,
     --NOME_PORTAFOGLIO nome assegnato al portafolgio
     nome_portafoglio varchar(255) NOT NULL
 );
--- transazione.sql
+++ transazione.sql
@@ -2,11 +2,16 @@
 
 CREATE TABLE transazione(
     --ID_TRANSAZIONE identifica univocamente una transazione
-    id_transazione int NOT NULL,
+    id_transazione int PRIMARY KEY,
     --TIPOLOGIA_TRANSAZIONE identifica la tipologia di transazione(entrata/uscita/trasferimento)
     tipologia_transazione tipologia_transazione,
     --CATEGORIA_TRASAZIONE identifica la vategoria della transazione(svago/tasse/affitto)
     categoria_transazione categoria_transazione,
     --VALORE identifica l'importo della transazione
-    valore decimal NOT NULL
+    valore decimal NOT NULL,
+    id_conto int,
+    CONSTRAINT FK_id_conto FOREIGN KEY(id_conto)
+        REFERENCES conto(id_conto)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
