aef4fd93e766479603525a59335329b114c80097
mrvittoriosomma <vittoriosomma21@gmail.com>
2024-01-01 10:54:27 +0100

    Aggiunto utente

--- inserimenti_utente.sql
+++ inserimenti_utente.sql
@@ -2,4 +2,5 @@
 
 INSERT INTO utente VALUES ('admin.admin@admin.admin', 'admin');
 INSERT INTO utente VALUES ('luca.rossi@email.it', 'LR.password12');
+INSERT into utente values ('mario.rossi@email.com', 'password');
  
