a10c129b8c4577e4dd3d40ab357181a2b0477b6a
mrvittoriosomma <vittoriosomma21@gmail.com>
2024-01-01 10:20:41 +0100

    Inseriti alcuni inserimenti di esempio

---
+++ inseriementi_portafoglio.sql
@@ -0,0 +1,4 @@
+--INSERIEMTNI DI ESEMPIO DELLA TABELLA PORTAFOGLIO
+INSERT INTO portafoglio VALUES(1, 'Personale');
+INSERT INTO portafoglio VALUES(2, 'Familiare');
+INSERT INTO portafoglio VALUES(3, 'Aziendale');
