c5047b310fdc1238f73fc7d7373121b05f5a1782
mrvittoriosomma <vittoriosomma21@gmail.com>
2024-01-01 10:34:53 +0100

    Aggiunta la tabella utente e relativi inserimenti

---
+++ inserimenti_utente.sql
@@ -0,0 +1,4 @@
+--INSERIMENTI DI ESEMPIO DELLA TABELLA UTENTI
+
+INSERT INTO utente VALUES ('admin.admin@admin.admin', 'admin');
+INSERT INTO utente VALUES ('luca.rossi@email.it', 'LR.password12');
---
+++ utente.sql
@@ -0,0 +1,7 @@
+--TABELLA DEGLI UTENTI
+CREATE TABLE utente(
+    email VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT validate_email
+        CHECK (email ~ '^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$')
+);
