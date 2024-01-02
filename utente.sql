01867ed0021baeb7d2cfc167c15262e4a89579ee
mrvittoriosomma <vittoriosomma21@gmail.com>
2024-01-02 20:23:21 +0100

    Inserito controllo di validazione della password

--- utente.sql
+++ utente.sql
@@ -4,7 +4,9 @@ CREATE TABLE utente(
     email VARCHAR(255) PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     CONSTRAINT validate_email
-        CHECK (email ~ '^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$')
+        CHECK (email ~ '^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$'),
+    CONSTRAINT validate_password
+        CHECK (password ~ '^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$')
 );
 
 --INSERIMENTI DI ESEMPIO DELLA TABELLA UTENTI
